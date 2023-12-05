3dec7758be257c882c90a471df164eeac99042e9
alexyl3 <122485096+alexyl3@users.noreply.github.com>
2023-12-05 13:34:18 +0300

    Новый столбец "time_of_day", анализ и устранение выбросов, проверка на ошибки в данных

--- Preparatory_analisis.py
+++ Preparatory_analisis.py
@@ -3,6 +3,22 @@ import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# функция для расчета тройного интерквартильного размаха
+def inq(col):
+    return df[col].quantile(.75) + (df[col].quantile(.75) - df[col].quantile(.25)) * 3
+
+# функция для добавления нового столбца в зависимости от времени суток визита
+def sort_by_time_of_day(time):
+    time = time.hour
+    if 10 > time >= 6:
+        return "утро"
+    if 17 > time >= 10:
+        return "день"
+    if 22 > time >= 17:
+        return "вечер"
+    if time >= 22 or time < 6:
+        return "ночь"
+
 
 df = pd.read_csv('ecom_raw.csv', sep=',', decimal=".")
 
@@ -12,7 +28,7 @@ df.columns = [x.lower().replace(" ", "_") for x in df.columns]
 # устранение пропусков по данным
 colours = ['#FFCF48', '#F5617D']
 sns.heatmap(df.isna(), cmap=sns.color_palette(colours), cbar=False)
-plt.savefig("heatmap_of_missing_data.jpg", bbox_inches="tight")
+plt.savefig("graphics/heatmap_of_missing_data.jpg", bbox_inches="tight")
 print(f'Строки с пропусками составляют {round(df["channel"].isna().value_counts()[True] / len(df["channel"]) * 100, 2)}%')
 print("Eсть пропуски в категориальных данных, пропущен регион, устройство и тип канала. "
       "\nТак как для ответа на основной вопрос исследования ключевыми являются регион и тип канала, "
@@ -23,7 +39,7 @@ df = df.dropna(how="any", subset=["channel", "region"])
 
 
 # столбец “payer” с информацией о том, является ли пользователь платящим или нет
-df["payer"] = df["revenue"].apply(lambda x: int(isnan(x)))
+df["payer"] = df["revenue"].apply(lambda x: int(not isnan(x)))
 
 # округление длительности сессии (так как все числа очень близки к целому, вероятно ошибка в процессе передачи)
 df["sessiondurationsec"] = df["sessiondurationsec"].apply(round)
@@ -46,16 +62,57 @@ df.loc[df["region"] == "Frаnce", "region"] = "France"
 df.loc[df["region"] == "Frаncе", "region"] = "France"
 df.loc[df["region"] == "UК", "region"] = "UK"
 
+# преобразование данных с временными значениями
 df["session_start"] = pd.to_datetime(df["session_start"])
 df["session_end"] = pd.to_datetime(df["session_end"])
 df["session_date"] = pd.to_datetime(df["session_date"])
 df["order_dt"] = pd.to_datetime(df["order_dt"])
-print(f'Исследуемый период с {max(df["session_start"]).date()} по {min(df["session_start"]).date()} продолжительностью'
-      f' {(max(df["session_start"]) - min(df["session_start"])).days} дней')
+
+
+# проверка на ошибки в данных с временем
+for index, row in df.iterrows():
+    row_has_mistake = 0
+    if row["session_start"] > row["session_end"] or row["sessiondurationsec"] < 0 :
+        row_has_mistake = 1
+    if row["session_date"].date() != row["session_start"].date():
+        row_has_mistake = 1
+    if row["month"] != row["session_date"].month or row["day"] != row["session_date"].day_of_week + 1:
+        row_has_mistake = 1
+    if row_has_mistake:
+        print("Обнаружена ошибка в данных")
+
+
+# период исследования
+print(f'Исследуемый период с {max(df["session_date"]).date()} по {min(df["session_date"]).date()} продолжительностью'
+      f' {(max(df["session_date"]) - min(df["session_date"])).days} дня')
+sns.displot(df["session_date"], bins=12)
+plt.savefig("graphics/session_date_spread.jpg")
+plt.clf()
+print("После построения гистограммы по месяцам видно, что количество записей распределены про месяцам ненормально, \nно при этом нет выбросов, поэтому стоит учитывать весть период.")
+print("- - -")
+
+# анализ на выбросы
+sns.boxplot(df["sessiondurationsec"])
+plt.savefig(f"graphics/boxplots_session_duration.jpg")
+plt.clf()
+sns.displot(df["day"], bins=7)
+plt.savefig("graphics/boxplots_day_of_week.jpg")
+plt.clf()
+print("Ярко выраженной сезонности не обнаружено, но есть выбросы в большую сторону в длительности сессиии.")
+print("Принято решение устранить их методом тройного интерквартильного размаха.")
+print("Значения выбросов заменены медианой по группе канала рекламы.")
+print("- - -")
+
+# устранение выбросов
+channels = list(set(df["channel"]))
+for channel_type in channels:
+    median = int(df.loc[df["channel"] == channel_type, "sessiondurationsec"].median())
+    df.loc[(df["channel"] == channel_type) & (df["sessiondurationsec"] > inq("sessiondurationsec")), "sessiondurationsec"] = median
+
+# столбец "time_of_day"
+df["time_of_day"] = df["session_start"].apply(sort_by_time_of_day)
+
 # ТЗ (невыполненные пункты).
-# определить исследуемый период, весь ли период брать для исследования или нет?
-# проверить данные на выбросы и адекватность данных
-# добавить столбец с указанием времени суток визита (утро 06:00-09:59, день 10:00-16:59, вечер 17:00-21:59, ночь 22:00-05:59)
 # Провести аналитический и графический анализ данных:
 # # Доля продаж по регионам
 # # Доля продаж по источникам
